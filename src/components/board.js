90cd55142521c24ec5626620c12f0accd511da06
svdunaev <54572104+svdunaev@users.noreply.github.com>
2020-05-28 23:19:51 +0500

    убирает ошибку из board.js

--- src/components/board.js
+++ src/components/board.js
@@ -17,7 +17,7 @@ export default class BoardComponent {
 
   getElement() {
     if (!this._element) {
-      this._element = createElement(this.getTemplate);
+      this._element = createElement(this.getTemplate());
     }
     return this._element;
   }
